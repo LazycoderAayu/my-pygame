610820e92a5ce8bfe8d80bf45bb05eacd32d3373
Aayush sonkar <153844634+LazycoderAayu@users.noreply.github.com>
2024-06-22 18:41:49 +0530

    Update SNAKE.py  (added high score and changed the colour's)

--- SNAKE.py
+++ SNAKE.py
@@ -1,17 +1,16 @@
 import pygame
-import time
 import random
+import os
 
 # Initialize pygame
 pygame.init()
 
 # Define colors
 white = (255, 255, 255)
-yellow = (255, 255, 102)
 black = (0, 0, 0)
 red = (213, 50, 80)
-green = (0, 255, 0)
-blue = (50, 153, 213)
+green = (34, 139, 34)  # Forest Green for the snake
+light_blue = (173, 216, 230)  # Light Blue for the background
 
 # Set display width and height
 dis_width = 600
@@ -27,12 +26,36 @@ snake_block = 10
 snake_speed = 15
 
 font_style = pygame.font.SysFont(None, 50)
+score_font = pygame.font.SysFont(None, 35)
+
+# Load or initialize high score
+if os.path.exists("high_score.txt"):
+    with open("high_score.txt", "r") as file:
+        high_score = int(file.read())
+else:
+    high_score = 0
+
+def our_snake(snake_block, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(dis, green, [x[0], x[1], snake_block, snake_block])
 
 def message(msg, color):
     mesg = font_style.render(msg, True, color)
     dis.blit(mesg, [dis_width / 6, dis_height / 3])
 
+def draw_score(score, high_score):
+    value = score_font.render("Score: " + str(score), True, black)
+    high_score_value = score_font.render("High Score: " + str(high_score), True, black)
+    dis.blit(value, [0, 0])
+    dis.blit(high_score_value, [0, 30])
+
+def place_food():
+    foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+    foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+    return foodx, foody
+
 def gameLoop():
+    global high_score
     game_over = False
     game_close = False
 
@@ -45,13 +68,14 @@ def gameLoop():
     snake_List = []
     Length_of_snake = 1
 
-    foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-    foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+    foodx, foody = place_food()
+
+    score = 0
 
     while not game_over:
 
         while game_close == True:
-            dis.fill(blue)
+            dis.fill(light_blue)
             message("You Lost! Press Q-Quit or C-Play Again", red)
             pygame.display.update()
 
@@ -84,8 +108,10 @@ def gameLoop():
             game_close = True
         x1 += x1_change
         y1 += y1_change
-        dis.fill(blue)
-        pygame.draw.rect(dis, green, [foodx, foody, snake_block, snake_block])
+        dis.fill(light_blue)
+
+        pygame.draw.rect(dis, red, [foodx, foody, snake_block, snake_block])
+
         snake_Head = []
         snake_Head.append(x1)
         snake_Head.append(y1)
@@ -97,17 +123,23 @@ def gameLoop():
             if x == snake_Head:
                 game_close = True
 
-        for block in snake_List:
-            pygame.draw.rect(dis, black, [block[0], block[1], snake_block, snake_block])
+        our_snake(snake_block, snake_List)
+        draw_score(score, high_score)
 
         pygame.display.update()
 
         if x1 == foodx and y1 == foody:
-            foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-            foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+            foodx, foody = place_food()
             Length_of_snake += 1
+            score += 10
+
+            if score > high_score:
+                high_score = score
 
         clock.tick(snake_speed)
+
+    with open("high_score.txt", "w") as file:
+        file.write(str(high_score))
 
     pygame.quit()
     quit()
